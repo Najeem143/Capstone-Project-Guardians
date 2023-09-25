e7210c38c829b60918f0a68ac7caf88bbbd9489f
najeem <najeem.afgs@gmail.com>
2023-09-25 11:36:18 -0700

    base UI issue

--- src/test/java/tek/sdet/framework/base/BaseUITest.java
+++ src/test/java/tek/sdet/framework/base/BaseUITest.java
@@ -11,14 +11,15 @@ public class BaseUITest extends CommonUtility {
 	public void setupTests() {
 		super.setupBrowser();
 	}
-
+	
 	@After
 	public void closeTests(Scenario scenario) {
-		if (scenario.isFailed()) {
-			byte[] screenShot = takeScreenShotAsBytes();
-			scenario.attach(screenShot, "image/png", scenario.getName() + " scenario failed");
+		if(scenario.isFailed()) {
+			byte[] screenshot = takeScreenShotAsBytes();
+			scenario.attach(screenshot, "image/png", scenario.getName()+" scenario failed");
 		}
 		super.quitBrowser();
 	}
+	
 
 }
